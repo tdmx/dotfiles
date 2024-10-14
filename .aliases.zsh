3d778ea07a59801c2e3e6a6534c7834f48ea147f
tdmx <51826571+tdmx@users.noreply.github.com>
2024-10-15 00:51:49 +0200

    feat: add nvim config as submodule

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,2 +1,4 @@
 alias la='ls -ltrah'
 alias dot='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+alias nvim='NVIM_APPNAME=nvim/main nvim'
+alias nvim-kick='NVIM_APPNAME=nvim/kickstart-modular-master nvim'
